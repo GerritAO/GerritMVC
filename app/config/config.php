db840ae7ee1816d407c8726626914986066deaa1
Gerrit Soerotaroeno <gerritsoerotaroeno@gmail.com>
2019-01-23 22:17:24 -0300

    url

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,8 @@
     define ('APPROOT', dirname(dirname(__FILE__ )));
     
     //ULRROOT
-    define ('URLROOT', 'http://localhost/projects/GerritMVC/');
+    // define ('URLROOT', 'http://localhost/projects/GerritMVC/');
+    define ('URLROOT', 'http://http://localhost/Github/GerritMVC/');
     
     //SiteName
     define ('SITENAME', 'GerritMVC');
